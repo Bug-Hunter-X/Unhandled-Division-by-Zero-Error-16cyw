63a5ea9a0f8453a2f88ace11845728887239acf8
BugHunter-X <askzenai@gmail.com>
2025-01-30 01:44:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,1 @@
+function add(a, b) {  return a + b; }  function subtract(a, b) {  return a - b;} function multiply(a, b) {  return a * b;} function divide(a, b) {  if (b === 0) {    return "Division by zero"; // Or another appropriate handling  }  return a / b;} let result = divide(10, 0); console.log(result); // Output: Division by zero
